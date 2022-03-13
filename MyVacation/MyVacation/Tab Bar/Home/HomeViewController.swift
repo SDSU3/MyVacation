bd189d74fde18bfc2837d8f14f9a270b74b095f4
salome sulkhanishvili <55408144+SalomeSulkhanishvili@users.noreply.github.com>
2022-03-14 02:15:43 +0400

    create home screen
    added:
    vacation collection view
    vacation cell
    segue to vacation detail
    buttons for user profile / add new vacation

--- MyVacation/MyVacation/Tab Bar/Home/HomeViewController.swift
+++ MyVacation/MyVacation/Tab Bar/Home/HomeViewController.swift
@@ -9,15 +9,71 @@ import UIKit
 
 class HomeViewController: MainViewController {
 
+    @IBOutlet weak var vacationCollectionView: UICollectionView!
+    @IBOutlet weak var addVacationButton: UIButton!
+    private let vacationDetailSegue = "moveToVacationDetail"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        vacationCollectionView.register(UINib(nibName: "VacationCell", bundle: nil), forCellWithReuseIdentifier: "VacationCell")
+        vacationCollectionView.delegate = self
+        vacationCollectionView.dataSource = self
+        
+        addVacationButton.layer.cornerRadius = 15
+        addVacationButton.clipsToBounds = true
+        
+    
+        addVacationButton.layer.masksToBounds = false
+        addVacationButton.layer.shadowColor = UIColor.gray.cgColor
+        addVacationButton.layer.shadowRadius = 3
+        addVacationButton.layer.shadowOpacity = 1
+        addVacationButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
     }
     
     static func load(with input: String) -> HomeViewController {
        let viewController = HomeViewController.loadFromStoryboard()
        return viewController
     }
+    
+    @IBAction func userProfileClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func addVacation(_ sender: UIButton) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == vacationDetailSegue {
+            if let viewController = segue.destination as? VacationDetailsViewController {
+                
+            }
+        }
+    }
+    
+}
 
+extension HomeViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5 // for testing purpose
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = vacationCollectionView.frame.width - 30
+        let height = width * 0.5
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = vacationCollectionView.dequeueReusableCell(withReuseIdentifier: "VacationCell", for: indexPath) as? VacationCell {
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: vacationDetailSegue, sender: indexPath)
+    }
+    
+    
 }
